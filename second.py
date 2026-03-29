e759d7bfbd2e80c0391eea58d0a20c8a114ffd88
DHRUV CHOUDHARY <dhruvchoudhary177@gmail.com>
2026-03-29 21:04:57 +0530

    Create second.py for matplotlib plotting
    Add a simple matplotlib plot with defined x and y values.

---
+++ second.py
@@ -0,0 +1,6 @@
+#This is the second program in the matplotlib libraray 
+import matplotlib.pyplot as plt
+x=[2,4,6,8]#This is the horizontal component.
+y=[34,30,78,67]#This is the horizontal component.
+plt.plot(x,y)#firstly we use the horizontal components 
+plt.show()
